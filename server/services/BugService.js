b9e28046dce18db6ae0ecd2d2535e0a4fae9ab30
Gabe McClaran <fitness-tech@hotmail.com>
2019-12-21 13:44:20 -0700

    backend passes test

--- server/services/BugService.js
+++ server/services/BugService.js
@@ -21,19 +21,21 @@ class BugService {
     return await _repository.create(rawData);
   }
   async edit(id, update) {
-    let data = await _repository.findOneAndUpdate({ _id: id }, update, {
-      new: true
-    });
+    let data = await _repository.findOneAndUpdate(
+      { _id: id },
+      {
+        closed: true
+      }
+    );
     if (!data) {
       throw new ApiError("Invalid ID", 400);
     }
-    return data;
   }
 
   async delete(id) {
     let data = await _repository.findOneAndUpdate(
       { _id: id },
-      { closed: false }
+      { closed: true }
     );
     if (!data) {
       throw new ApiError("invalid ID", 400);
